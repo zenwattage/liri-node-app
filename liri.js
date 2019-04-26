b36fa6e10af8c3603ce106a3e08cb23941c2c485
Scott H <zenwattage@gmail.com>
2019-04-26 08:20:19 -0700

    initial linkup of project instructions complete

--- liri.js
+++ liri.js
@@ -2,5 +2,10 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
+console.log(process.argv[2]);
 
+//concert-this
+//spotify-this-song
+//movie-this
+//do-what-it-says
