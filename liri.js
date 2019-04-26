130467bf08c311626fc5bacd60f40ea625c53ce2
Scott H <zenwattage@gmail.com>
2019-04-26 12:40:52 -0700

    setting up constants and variables for axios,spotify,moment, and filesystem(fs)

--- liri.js
+++ liri.js
@@ -1,11 +1,22 @@
+// our local environmental config file - gitignored
 require("dotenv").config();
 
+// axios required for calls to the various API's
+const axios = require('axios');
+// file system interaction package
+const fs = require('fs');
+// gitignored keys for apis
 var keys = require("./keys.js");
+// moment npm for date formatting
+var moment = require('moment');
 
-var spotify = new Spotify(keys.spotify);
+//moment().format();
+
+//var spotify = new Spotify(keys.spotify);
 console.log(process.argv[2]);
 
 //concert-this
 //spotify-this-song
 //movie-this
 //do-what-it-says 
+
