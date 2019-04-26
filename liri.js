da8256a69df5f20a8262153648d16cb45c0880d1
Scott H <zenwattage@gmail.com>
2019-04-26 16:12:29 -0700

    bandinstown url from instructions added

--- liri.js
+++ liri.js
@@ -7,36 +7,29 @@ var keys = require("./keys.js");
 // axios required for calls to the various API's
 const axios = require('axios');
 
-// node-spotify-api for search and request methods (thanks Denis)
+// node-spotify-api for search and request methods (thanks Denis Molloy)
 var Spotify = require('node-spotify-api');
-
+//create spotify object
+var spotify = new Spotify(keys.spotify);
 // file system interaction package
 const fs = require('fs');
-
 // moment npm for date formatting
 var moment = require('moment');
+
+//bandsintown
+//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
 
 //omdb
 // http://www.omdbapi.com/?i=tt3896198&apikey=2e1c1418
 
-var spotify = new Spotify(keys.spotify);
-console.log(spotify);
 
 
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-    console.log(data); 
-  })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
-  });
 //console.log(process.argv[2]);
 
 //concert-this
 
-//function to bandsintown api
+//function to call bandsintown api
 //axios get call 
 /* 
 function concertThis() {
@@ -46,16 +39,29 @@ function concertThis() {
 }
  */
 
- // Then run a request with axios to the OMDB API with the movie specified
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-    function(response) {
-      console.log("The movie's rating is: " + response.data.imdbRating);
-    }
-  );
   
 
 
 //spotify-this-song
+spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+  });
+
+
 //movie-this
+
+ // Then run a request with axios to the OMDB API with the movie specified
+ axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+    function(response) {
+      console.log("The movie's rating is: " + response.data.imdbRating);
+    }
+  );
+
+
 //do-what-it-says 
 
