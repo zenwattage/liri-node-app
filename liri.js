40e0e452b5e1aed42005e931b36bb855afffd382
Scott H <zenwattage@gmail.com>
2019-04-26 21:26:39 -0700

    getting spotify-this-song built

--- liri.js
+++ liri.js
@@ -9,8 +9,7 @@ const axios = require('axios');
 
 // node-spotify-api for search and request methods (thanks Denis Molloy)
 var Spotify = require('node-spotify-api');
-//create spotify object with keys linked
-var spotify = new Spotify(keys.spotify);
+
 
 // file system interaction package
 const fs = require('fs');
@@ -34,50 +33,58 @@ var moment = require('moment');
 
 //function to call bandsintown api
 //axios get call 
-
+/* 
 function concertThis(artist) {
-//axios call to  bandsintown api
-axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-    .then(function (response) {
-        console.log(response);
-        console.log(response.data);
-        //iterate over response object to get name/location/date of event
-        for (key of response.data) {
-
-            console.log(key.venue.name);
-            console.log(key.venue.city + ',' + key.venue.region);
-            console.log(moment(key.datetime).format("MM/DD/YYYY"));
-        }
-    }).catch(function (error) {
-        console.log(error);
+    //axios call to  bandsintown api
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            //console.log(response);
+            //console.log(response.data);
+            //iterate over response object to get name/location/date of event
+            for (key of response.data) {
+                console.log("Appearing at the: " + key.venue.name);
+                console.log("              At: " + key.venue.city + ', ' + key.venue.region);
+                console.log("              On: " + moment(key.datetime).format("MM/DD/YYYY"));
+                console.log("\n");
+            }
+        }).catch(function (error) {
+            console.log(error);
         });
 }
-concertThis("Blink182");
+ */
+//concertThis("Blink182");
 
 
 
 
 //spotify-this-song
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
+
+function spotifyThisSong(song) {
+    //create spotify object with keys linked
+    var spotify = new Spotify(keys.spotify);
 
 
-//movie-this
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 
-// Then run a request with axios to the OMDB API with the movie specified
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-    function (response) {
-        console.log("The movie's rating is: " + response.data.imdbRating);
-    })
-    .catch(function (error) {
-    console.log(error);
-    });
+
+    //movie-this
+
+    // Then run a request with axios to the OMDB API with the movie specified
+    axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            console.log("The movie's rating is: " + response.data.imdbRating);
+        })
+        .catch(function (error) {
+            console.log(error);
+        }
+        );
 
 
 //do-what-it-says 
