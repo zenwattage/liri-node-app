6b9fa7108c5db4cd4e2cbc26af704304f25c88f1
Scott H <zenwattage@gmail.com>
2019-05-01 09:38:13 -0700

    adding bonus writeLog function

--- liri.js
+++ liri.js
@@ -160,7 +160,20 @@ function doWhatItSays() {
 }; //end dowhatitsays
 
 
-// 
+// write
+// This block of code will create a file called "movies.txt".
+// It will then print "Inception, Die Hard" in the file
+fs.writeFile("movies.txt", "Inception, Die Hard", function(err) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (err) {
+      return console.log(err);
+    }
+  
+    // Otherwise, it will print: "movies.txt was updated!"
+    console.log("movies.txt was updated!");
+  
+  });
 
 
 
