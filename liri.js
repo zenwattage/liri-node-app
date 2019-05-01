0c0db77a78fcbdbbec7d64ce52baf55c45f8067b
Scott H <zenwattage@gmail.com>
2019-05-01 09:40:56 -0700

    log.txt writefile function edit

--- liri.js
+++ liri.js
@@ -161,17 +161,17 @@ function doWhatItSays() {
 
 
 // write
-// This block of code will create a file called "movies.txt".
-// It will then print "Inception, Die Hard" in the file
-fs.writeFile("movies.txt", "Inception, Die Hard", function(err) {
+// This block of code will create a file called "log.txt".
+// It will then print MYOUTPUT in the file
+fs.writeFile("log.txt", "MYOUTPUT", function(err) {
 
     // If the code experiences any errors it will log the error to the console.
     if (err) {
       return console.log(err);
     }
   
-    // Otherwise, it will print: "movies.txt was updated!"
-    console.log("movies.txt was updated!");
+    // Otherwise, it will print: "log.txt was updated!"
+    console.log("log.txt was updated!");
   
   });
 
